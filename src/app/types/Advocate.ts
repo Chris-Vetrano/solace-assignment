349ffae2425a32a50af2f05f41852df5ce8dc6df
Chris Vetrano <crvetrano@gmail.com>
2025-01-20 06:08:22 -0500

    [SOL-1] Increase Type Safety - Add Advocate type - Use Advocate type in page.tsx - Ensure onChange is typed properly

---
+++ src/app/types/Advocate.ts
@@ -0,0 +1,9 @@
+export default interface Advocate {
+  firstName: string;
+  lastName: string;
+  city: string;
+  degree: string;
+  specialties: string[];
+  yearsOfExperience: number;
+  phoneNumber: number;
+}
