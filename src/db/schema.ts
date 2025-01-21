5e5ed4dda2766c2b8c05d0f06ab39aff153e348c
Chris Vetrano <crvetrano@gmail.com>
2025-01-20 19:46:09 -0500

    [SOL-1] Setup migrations/schema for indexes - Add indexes for basic text search - Starts on the work for implementing a search endpoint

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,18 +7,27 @@ import {
   serial,
   timestamp,
   bigint,
+  index,
 } from "drizzle-orm/pg-core";
 
-const advocates = pgTable("advocates", {
-  id: serial("id").primaryKey(),
-  firstName: text("first_name").notNull(),
-  lastName: text("last_name").notNull(),
-  city: text("city").notNull(),
-  degree: text("degree").notNull(),
-  specialties: jsonb("payload").default([]).notNull(),
-  yearsOfExperience: integer("years_of_experience").notNull(),
-  phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
-  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
-});
-
-export { advocates };
+export const advocates = pgTable(
+  "advocates",
+  {
+    id: serial("id").primaryKey(),
+    firstName: text("first_name").notNull(),
+    lastName: text("last_name").notNull(),
+    city: text("city").notNull(),
+    degree: text("degree").notNull(),
+    specialties: jsonb("payload").default([]).notNull(),
+    yearsOfExperience: integer("years_of_experience").notNull(),
+    phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
+    createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
+  },
+  (table) => ({
+    nameIdx: index("advocates_name_idx").on(table.firstName, table.lastName),
+    cityIdx: index("advocates_city_idx").on(table.city),
+    degreeIdx: index("advocates_degree_idx").on(table.degree),
+    yearsExpIdx: index("advocates_years_exp_idx").on(table.yearsOfExperience),
+    createdAtIdx: index("advocates_created_at_idx").on(table.createdAt),
+  })
+);
